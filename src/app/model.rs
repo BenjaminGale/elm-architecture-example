4b7250bd137f0cd96903bd30cba4acf12a41d507
Benjamin Gale <benjaminiangale@gmail.com>
2026-03-12 08:14:03 +0000

    Seperate event handling logic

--- src/app/model.rs
+++ src/app/model.rs
@@ -15,11 +15,17 @@ impl AppModel {
     pub fn update(self: &mut Self, event: &Event) {
         match event {
             Event::App(AppEvent::Init) => return,
-            Event::Counter(CounterEvent::Increment) => self.count += 1,
-            Event::Counter(CounterEvent::Decrement) => self.count -= 1,
+            Event::Counter(ev) => self.update_count(ev),
         }
     }
-    
+
+    fn update_count(self: &mut Self, event: &CounterEvent) {
+        match event {
+            CounterEvent::Increment => self.count += 1,
+            CounterEvent::Decrement => self.count -= 1,
+        }
+    }
+
     pub fn format_count(self: &Self) -> String {
         format!("Count: {}", self.count)
     }
